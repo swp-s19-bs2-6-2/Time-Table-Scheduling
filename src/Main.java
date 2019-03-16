e9aebe0acebd96afa290a2aa1b6392ab52789843
Almir Mullanurov <almirka02261999@gmail.com>
2019-03-16 14:02:27 +0300

    added /src

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = Collections.min(Arrays.asList(1, 2, 3, 4));
+        System.out.println(x);
+    }
+}
