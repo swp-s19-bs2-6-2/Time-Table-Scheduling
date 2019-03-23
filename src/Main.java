13f02513b3c37c4aa1cec43f7b449a9486d832cc
fniyaz <35842362+fniyaz@users.noreply.github.com>
2019-03-23 21:04:31 +0300

    CSV parsing's done for TimeSlots, SlassRooms and Courses

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
-import java.util.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
 
-    public static void main(String[] args) throws CloneNotSupportedException {
-        List<Lesson> availableLessons = new ArrayList<>();
+    public static void main(String[] args) throws CloneNotSupportedException, IOException, Parser.IncorrectFileStructureException {
+        List<Lesson> l = Parser.getLessons(new Parser(new FileInputStream("courses.csv")));
+
+        List<Classroom> c = Parser.getClassRooms(new Parser(new FileInputStream("classrooms.csv")));
+
+        List<TimeSlot> s = Parser.getTimeSlots(new Parser(new FileInputStream("timeSlots.csv")), c);
+
+        TimeTable timeTable = new TimeTable(s, 5, l);
+        timeTable.printTimeTable();
+
+        /*List<Lesson> availableLessons = new ArrayList<>();
         for(int i = 0; i<3; i++){
             Course course = new Course(Integer.toString(i));
             availableLessons.add(new Lesson(course));
@@ -18,6 +29,6 @@ public class Main {
             availableTimeSlots.add(new TimeSlot(i, 0, i + 1, 0, availableClassrooms));
         }
         TimeTable timeTable = new TimeTable(availableTimeSlots, 5, availableLessons);
-        timeTable.printTimeTable();
+        timeTable.printTimeTable();*/
     }
 }
