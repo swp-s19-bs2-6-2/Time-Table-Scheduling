25e98563a82d518a0f2aec0983038b1513d5d378
Almir Mullanurov <almirka02261999@gmail.com>
2019-04-01 14:21:30 +0300

    Added workload balancing algorithm Added some cloneable methods Now algorithm finds many possible schedules

---
+++ src/Teacher.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class Teacher {
+    private int teacherID;
+
+    private String name;
+    private List <TimeSlot> preferredTimeslots;
+
+    // Constructors
+
+    public Teacher(int teacherID, String name){
+        this.teacherID = teacherID;
+        this.name = name;
+        this.preferredTimeslots = new ArrayList<>();
+    }
+
+    public Teacher(int teacherID, String name, List<TimeSlot> preferredTimeslots) {
+        this.teacherID = teacherID;
+        this.name = name;
+        this.preferredTimeslots = preferredTimeslots;
+    }
+
+    // Getters and setters
+
+    public int getTeacherID() {
+        return teacherID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<TimeSlot> getPreferredTimeslots() {
+        return preferredTimeslots;
+    }
+
+    public boolean setPreferredTimeslots(List<TimeSlot> preferredTimeslots) {
+        if (!checkTimeslots(preferredTimeslots)){
+            return false;
+        }
+        this.preferredTimeslots = preferredTimeslots;
+        return true;
+    }
+
+    // Time slots operations
+
+    private boolean checkTimeslots(Collection <TimeSlot> timeSlots){
+        Set<TimeSlot> timeSlotsSet = new HashSet<>();
+        for (TimeSlot t : timeSlots){
+            if (timeSlotsSet.contains(t)){
+                return false;
+            }
+            timeSlotsSet.add(t);
+        }
+        return true;
+    }
+
+    public boolean addTimeslots(Collection <TimeSlot> timeSlots){
+        if (!checkTimeslots(timeSlots)){
+            return false;
+        }
+        for (TimeSlot t : timeSlots){
+            if (this.preferredTimeslots.contains(t)){
+                return false;
+            }
+        }
+//        for (TimeSlot t : timeSlots){
+//            this.preferredTimeslots.add(t);
+//        }
+        this.preferredTimeslots.addAll(timeSlots);
+        return true;
+    }
+
+    public boolean addSingleTimeslot(TimeSlot timeSlot){
+        if (this.preferredTimeslots.contains(timeSlot)){
+            return false;
+        }
+        preferredTimeslots.add(timeSlot);
+        return true;
+    }
+}
