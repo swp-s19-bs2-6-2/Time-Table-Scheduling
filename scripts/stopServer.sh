e50ebbdb292bc977cc7e4d30fd396a96d3a9d93d
PolarNorth <antonov9312@yandex.ru>
2019-04-28 18:06:06 +0300

    Stop server is temporary removed

--- scripts/stopServer.sh
+++ scripts/stopServer.sh
@@ -5,3 +5,10 @@ pid=$(lsof -i:5000 -t); if [[ "$(expr length "$pid")" -ne "0" ]]; then
 kill -KILL $pid; else
 echo 'No running server found'
 fi
+
+#stage('Stop server') {
+#             steps {
+#                 sh 'chmod +x ./scripts/stopServer.sh'
+#                 sh './scripts/stopServer.sh'
+#             }
+#         }
