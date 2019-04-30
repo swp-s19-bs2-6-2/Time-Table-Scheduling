f080b77a3198f74fb5ea9bba74297d41dc11d874
PolarNorth <antonov9312@yandex.ru>
2019-04-30 22:26:18 +0300

    Jenkins: stopServer small fix

--- scripts/stopServer.sh
+++ scripts/stopServer.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-echo 'Stop running server first'
+echo 'Stop running server first';
 pid=$(lsof -i:8081 -t);
-echo "PID is "
-echo $pid
-echo "PID length is"
-echo expr length "$pid"
-echo "Attempting to kill"
+echo "PID is ";
+echo $pid;
+echo "PID length is";
+echo expr length "$pid";
+echo "Attempting to kill";
 if [[ "$(expr length "$pid")" -ne "0" ]]; then
 kill -KILL $pid; else
 echo 'No running server found'
