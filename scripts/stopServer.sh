c19ac04dfadb44908c5002fe2deded9dd0073b71
PolarNorth <antonov9312@yandex.ru>
2019-04-30 22:07:37 +0300

    Jenkins: stopServer is returned

--- scripts/stopServer.sh
+++ scripts/stopServer.sh
@@ -5,10 +5,3 @@ pid=$(lsof -i:8081 -t); if [[ "$(expr length "$pid")" -ne "0" ]]; then
 kill -KILL $pid; else
 echo 'No running server found'
 fi
-
-#stage('Stop server') {
-#             steps {
-#                 sh 'chmod +x ./scripts/stopServer.sh'
-#                 sh './scripts/stopServer.sh'
-#             }
-#         }
